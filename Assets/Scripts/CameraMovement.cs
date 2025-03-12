1015ddc2dbbb6f8f6f1326587295d7b45b556d03
SpertixMato <mato@spertix.com>
2025-03-12 22:38:12 +0100

    some changes

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -8,10 +8,11 @@ public class CameraMovement : MonoBehaviour
     public Transform LookTarget;
 
     public float Smoothnes = 0.5f;
+    private Vector3 _velocity;
 
     void LateUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, Target.position, Smoothnes * Time.deltaTime);
+        transform.position = Vector3.SmoothDamp(transform.position, Target.position, ref _velocity, Smoothnes * Time.deltaTime);
         Vector3 direction = LookTarget.position - transform.position;
         transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
     }
